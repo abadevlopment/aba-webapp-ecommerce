d574dc7a0a6936db26854b1d560f2552904b7ccb
abadevlopment <92816835+abadevlopment@users.noreply.github.com>
2022-02-22 01:16:46 -0500

    2/22/2022

---
+++ src/products/ProductsTest.js
@@ -0,0 +1,119 @@
+// const products = [
+//     { id: 1, title: "Arduino DUE Original", category: "Tarjetas", price: 190 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743443/ARDUINO/due_gpuzzq.jpg" } , 
+//     { id: 2, title: "Arduino M0", category: "Tarjetas", price: 149 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743498/ARDUINO/arduinoM0_tpjpml.jpg" } , 
+//     { id: 3, title: "Arduino Mega R3 Original", category: "Tarjetas", price: 175 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743546/ARDUINO/ArduinoMegaOriginal_bao6zm.jpg" } , 
+//     { id: 4, title: "Arduino Micro", category: "Tarjetas", price: 110 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743585/ARDUINO/ArduinoMicro_eufjcp.jpg" } , 
+//     { id: 5, title: "Arduino Mini Pro 5V 16Mhz", category: "Tarjetas", price: 22 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743631/ARDUINO/ArduinoMini_vtocys.jpg" } , 
+//     { id: 6, title: "Arduino MKR WiFi 1010", category: "Tarjetas", price: 169 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743741/ARDUINO/Arduino_MKR_aeplun.jpg" } , 
+//     { id: 7, title: "Arduino Starter Kit Original", category: "Tarjetas", price: 459 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743782/ARDUINO/ArduinoStarterKit_mrloqz.jpg" } , 
+//     { id: 8, title: "Arduino UNO R3 Original", category: "Tarjetas", price: 95 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743840/ARDUINO/UNO_ML1_d3zbf4.jpg" } , 
+//     { id: 9, title: "DFRduino MEGA ADK", category: "Tarjetas", price: 155 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636744044/ARDUINO/DFRduino_MEGA_ADK_ezrhhf.jpg" } , 
+//     { id: 10, title: "Grove Speech recognizer Kit", category: "Kits", price: 189 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650114/ARDUINO/grovespeechkit_ytu1dx.jpg" } , 
+//     { id: 11, title: "Kit basico del ESP32", category: "Kits", price: 85 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650377/ARDUINO/kitbasicoESP32-2_yz6b9z.jpg" } , 
+//     { id: 12, title: "Kit de Robotica de Arduino", category: "Kits", price: 135 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650666/ARDUINO/kit_robotica_se8gtz.jpg" } , 
+//     { id: 13, title: "Kit Educativo de Arduino", category: "Kits", price: 182 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650903/ARDUINO/Kit_educativo_Arduino-1_dityyz.jpg" } , 
+//     { id: 14, title: "Kit Intermedio de Arduino", category: "Kits", price: 129 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636651130/ARDUINO/kit_intermedio_arduino_szg9jq.jpg" } , 
+//     { id: 15, title: "Makey Makey Classic by JoyLabz", category: "Kits", price: 199 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636651277/ARDUINO/makeymakey1_uqwncb.jpg" } , 
+//     { id: 16, title: "Chasis 4WD Kit – Basic", category: "Robotica", price: 110 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652009/ARDUINO/4wdkit_ifrw5d.jpg" } , 
+//     { id: 17, title: "Chasis Tank (Tanque)", category: "Robotica", price: 155 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652379/ARDUINO/tank1_ni17bj.jpg" } , 
+//     { id: 18, title: "Controlador de servos PCA9685", category: "Robotica", price: 24 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652453/ARDUINO/PCA9685_yubqfh.jpg" } , 
+//     { id: 19, title: "Dagu 2WD Self-Balancing Chassis", category: "Robotica", price: 149 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652631/ARDUINO/ChasisBalancing_dhezxg.jpg" } , 
+//     { id: 20, title: "Garra robotica", category: "Robotica", price: 25 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636653143/ARDUINO/garrarobotica_i87sv9.jpg" } , 
+//     { id: 21, title: "Hexapod Chassis Kit", category: "Robotica", price: 299 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636653324/ARDUINO/spider_h4h7fq.jpg" } 
+// ]
+const products = [
+    { id: 1, title: "Arduino DUE Original", category: "Tarjetas", price: 1900 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743443/ARDUINO/due_gpuzzq.jpg" } , 
+    { id: 2, title: "Arduino M0", category: "Tarjetas", price: 149 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743498/ARDUINO/arduinoM0_tpjpml.jpg" } , 
+    { id: 3, title: "Arduino Mega R3 Original", category: "Tarjetas", price: 17500 , stock: 25 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636743546/ARDUINO/ArduinoMegaOriginal_bao6zm.jpg" } , 
+    { id: 4, title: "Grove Speech recognizer Kit", category: "Kits", price: 189 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650114/ARDUINO/grovespeechkit_ytu1dx.jpg" } , 
+    { id: 5, title: "Kit basico del ESP32", category: "Kits", price: 85 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650377/ARDUINO/kitbasicoESP32-2_yz6b9z.jpg" } , 
+    { id: 6, title: "Kit de Robotica de Arduino", category: "Kits", price: 135 , stock: 30 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636650666/ARDUINO/kit_robotica_se8gtz.jpg" } , 
+    { id: 7, title: "Chasis 4WD Kit - Basic", category: "Robotica", price: 9 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652009/ARDUINO/4wdkit_ifrw5d.jpg" } , 
+    { id: 8, title: "Chasis Tank (Tanque)", category: "Robotica", price: 155 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652379/ARDUINO/tank1_ni17bj.jpg" } , 
+    { id: 9, title: "Controlador de servos PCA9685", category: "Robotica", price: 4 , stock: 15 , pictureUrl: "https://res.cloudinary.com/dvinxey2w/image/upload/v1636652453/ARDUINO/PCA9685_yubqfh.jpg" } , 
+]
+
+
+export const getFetch = new Promise( (resolve, reject) => {
+
+    const condition = true;
+    if (condition) {
+        setTimeout( () => {
+            resolve(products)
+        }, 2000 )
+    }
+    else {
+        setTimeout( () => {
+            reject('404 not found')
+        }, 2000 )
+    }
+} )
+
+
+// export const getFetch2 = new Promise( (resolve, reject) => {
+
+//     const condition = true;
+//     if (condition) {
+//         setTimeout( () => {
+
+//             resolve(desc)
+//         }, 2000 )
+
+//     }
+//     else {
+//         setTimeout( () => {
+
+//             reject('404 not found')
+//         }, 2000 )
+//     }
+// } )
+
+// import { useEffect, useState } from 'react';
+// import { getFetch } from '../Products/getFetch';
+// import { useParams } from 'react-router';
+// import ItemList from './ItemList/ItemList';
+// import './ItemListContainer.scss'
+
+
+// function ItemListContainer ({greeting}) {
+//     const [product, setProduct] = useState([])
+//     const [loading, setLoading] = useState(true)
+//     const {categoryID} = useParams()
+
+//     useEffect(()=>{
+//         if (categoryID) {
+//             getFetch
+//             .then( res => {
+//                 setProduct(res.filter((data) => data.category === categoryID))
+//             })
+//             .catch()
+//             .finally( () => setLoading(false) )
+//         } else {
+//             getFetch
+//             .then( res => {
+//                 setProduct(res)
+//             })
+//             .catch()
+//             .finally( () => setLoading(false) )
+//         }
+
+//     },[categoryID])
+
+//     return (
+//         <section className="ilc" id="main">
+//             <div id="greet" ><h1>{greeting}</h1></div>
+//             <div id="catalog" >
+//                 { loading ? 
+//                 <>
+//                     <div className="btn btn-primary" >
+//                         <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+//                         <span className="sr-only">Cargando...</span>
+//                     </div>
+//                 </>
+//                 :
+//                  <ItemList items= {product} />}                
+//             </div>   
+//         </section>
+//     )
+// }
+// export default ItemListContainer
